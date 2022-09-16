9942be13d50feb25f7baf5b4faa98be3ba271d37
tom <toms@live.jp>
2022-09-16 13:37:03 +0900

    footer vue is added

---
+++ src/client/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <a
+    href="https://www.npmjs.com/package/electronade"
+    class="footer-link"
+    target="_blank"
+  >
+    <span class="text">npm electronade</span>
+    <span class="material-icons icon">open_in_new</span>
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
