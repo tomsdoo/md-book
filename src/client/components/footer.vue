6b2093b20fd8f68381f7ecc07af0312910d61cd8
tom <toms@live.jp>
2022-09-16 18:26:23 +0900

    footer style

--- src/client/components/footer.vue
+++ src/client/components/footer.vue
@@ -14,3 +14,18 @@ import { defineComponent } from "vue";
 
 export default defineComponent({});
 </script>
+
+<style scoped>
+.footer-link {
+  display: grid;
+  grid-template: "text icon" max-content / max-content max-content;
+  align-items: center;
+}
+.footer-link > .text {
+  grid-area: text;
+}
+.footer-link > .icon {
+  grid-area: icon;
+  font-size: 1em;
+}
+</style>
