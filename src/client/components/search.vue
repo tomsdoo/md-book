1119d2db786c3d77aa6bffb8f4b71652275d767e
tom <toms@live.jp>
2022-09-16 18:59:35 +0900

    search result style is added

--- src/client/components/search.vue
+++ src/client/components/search.vue
@@ -1,32 +1,33 @@
 <template>
   <vue-layout :indexed-page-contents="indexedPageContents">
-    <div class="search-result">
-      <h1 class="title">
-        "{{ keyword }}": searchResultPages.length
-      </h1>
-      <ul class="list">
-        <li
-          v-for="(page, index) in searchResultPages"
-          :key="index"
-          class="item"
-        >
-          <router-link
-            :to="{ name: 'article', query: { path: page.rawPath } }"
-            class="link"
+    <transition name="fade">
+      <div v-if="state.ready" class="search-result">
+        <h1 class="title">
+          "{{ keyword }}": {{ searchResultPages.length }}
+        </h1>
+        <ul class="list">
+          <li
+            v-for="(page, index) in searchResultPages"
+            :key="index"
+            class="item"
           >
-            <span class="material-icons-outlined icon">article</span>
-            <span class="text">{{ page.title }}</span>
-          </router-link>
-        </li>
-      </ul>
-      search result {{ route.query }}
-    </div>
+            <router-link
+              :to="{ name: 'article', query: { path: page.rawPath } }"
+              class="link"
+            >
+              <span class="material-icons-outlined icon">article</span>
+              <span class="text">{{ page.title }}</span>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </transition>
   </vue-layout>
 </template>
 
 <script lang="ts">
 import VueLayout from "./layout.vue";
-import { computed, defineComponent, SetupContext } from "vue";
+import { computed, defineComponent, nextTick, reactive, SetupContext } from "vue";
 import { useRoute } from "vue-router";
 
 export default defineComponent({
@@ -44,19 +45,63 @@ export default defineComponent({
     }
   },
   setup(props){
+    const state = reactive({
+      ready: false
+    });
     const route = useRoute();
     const keyword = computed(() => route.query.keyword);
-    console.log(keyword);
+
     const searchResultPages = computed(() => {
       const keyl = route.query.keyword.split(" ");
       return props.pageContents
         .filter(({ text }) => keyl.every(keye => text.match(new RegExp(keye, "i"))));
     });
+    nextTick(() => {
+      state.ready = true;
+    });
     return {
-      route,
+      state,
       keyword,
       searchResultPages
     };
   }
 });
 </script>
+
+<style scoped>
+.search-result {
+  display: grid;
+  grid-gap: 1rem;
+}
+
+.search-result > .title {
+  font-size: 20px;
+}
+
+.search-result > .list {
+  display: grid;
+  grid-gap: 0.5rem;
+}
+
+.search-result > .list > .item {
+}
+
+.search-result > .list > .item > .link {
+  display: grid;
+  grid-template: "icon text" max-content / max-content max-content;
+  grid-gap: 0.5em;
+  align-items: start;
+  color: #439cff;
+  width: max-content;
+}
+.search-result > .list > .item > .link:hover {
+  text-shadow: 0 0 0.5px;
+}
+
+.search-result > .list > .item > .link > .icon {
+  font-size: inherit;
+  width: 1em;
+  height: 1em;
+  padding-top: 0.2em;
+}
+</style>
