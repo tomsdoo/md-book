1f39fd959b4594005b27bb6cb2d19174a010891d
tom <toms@live.jp>
2022-09-16 13:36:30 +0900

    header vue is added

---
+++ src/client/components/header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header-content">
+    <span>electronade</span>
+    <form
+      onsubmit="return false"
+      v-on:submit="onSubmit"
+      class="key-form"
+    >
+      <input id="keybox" v-model="state.keyword" />
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  setup(){
+    const router = useRouter();
+    const state = reactive({
+      keyword: ""
+    });
+
+    const onSubmit = () => {
+      if(!Boolean(state.keyword)){return;}
+
+      router.push({
+        name: "search",
+        query: { keyword: state.keyword }
+      });
+      state.keyword = "";
+    };
+    return {
+      state,
+      onSubmit
+    };
+  }
+});
+</script>
