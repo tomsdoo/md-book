6df6e8b818c1124b01fbe20a9a8778c2996c85a7
tom <toms@live.jp>
2022-09-16 18:26:36 +0900

    header style

--- src/client/components/header.vue
+++ src/client/components/header.vue
@@ -6,7 +6,7 @@
       v-on:submit="onSubmit"
       class="key-form"
     >
-      <input id="keybox" v-model="state.keyword" />
+      <input class="keybox" v-model="state.keyword" />
     </form>
   </div>
 </template>
@@ -38,3 +38,39 @@ export default defineComponent({
   }
 });
 </script>
+
+<style scoped>
+.header-content {
+  position: relative;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  width: min(1100px, calc(100vw - 2rem));
+  margin: 0 auto;
+  height: 100%;
+}
+
+.header-content > .key-form {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  display: grid;
+  justify-content: end;
+  align-items: center;
+  width: 10rem;
+}
+
+.header-content > .key-form > .keybox {
+  width: 100%;
+  border-radius: 0.5em;
+  box-shadow: 0 0 1px;
+  padding: 0.2em 0.5em;
+}
+
+@media screen and (max-width: 600px) {
+  .header-content > .key-form {
+    width: 5rem;
+  }
+}
+</style>
