250eefecb42c8d72dc3cfb33b6edb3af7edbb2e3
tom <toms@live.jp>
2022-09-18 07:37:22 +0900

    document server code is moved

---
+++ src/bin/opts/serve.ts
@@ -0,0 +1,42 @@
+import Koa from "koa";
+// @ts-expect-error
+import Router from "koa-router";
+// @ts-expect-error
+import serve from "koa-static-server";
+import * as path from "path";
+import * as fs from "fs";
+
+
+export async function serveDocuments(directoryPath: string){
+  ///
+
+  const app = new Koa();
+  const router = new Router();
+
+  const port = Number(process.env.PORT ?? 3000);
+
+  async function indexhtml(ctx: Koa.Context): Promise<any> {
+    ctx.response.status = 200;
+    ctx.response.type = "text/html";
+    ctx.response.body = await fs.promises.readFile(
+      path.join(directoryPath, "./index.html"),
+      "utf8"
+    );
+  }
+
+  router.get("/", indexhtml);
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+
+  app.use(
+    serve({
+      rootDir: directoryPath,
+    })
+  );
+
+  app.listen(port, () => {
+    console.log(`listening ${port}`);
+  });
+
+  ///
+}
