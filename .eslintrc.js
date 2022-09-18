2c2f839e7ed459339eef378be6137ad8cb8325a0
tom <toms@live.jp>
2022-09-18 17:28:07 +0900

    eslint is installed

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard-with-typescript',
+    'prettier'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.base.json'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
