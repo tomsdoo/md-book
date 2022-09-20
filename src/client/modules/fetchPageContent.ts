acbe650bf7187d5c84d32ebcb6742b253dbb145b
tom <toms@live.jp>
2022-09-21 05:59:29 +0900

    fetchPageContent() is moved out of client/index.ts

---
+++ src/client/modules/fetchPageContent.ts
@@ -0,0 +1,28 @@
+import * as marked from "marked";
+
+export interface PageContent {
+  indexed: boolean;
+  rawPath: string;
+  url: string;
+  text: string;
+  title: string;
+  html: string;
+}
+
+export async function fetchPageContent({
+  path,
+  indexed,
+}): Promise<PageContent> {
+  return await fetch(path)
+    .then(async (response) => ({
+      indexed,
+      rawPath: path,
+      url: response.url,
+      text: await response.text(),
+    }))
+    .then((content) => ({
+      ...content,
+      title: content.text.split("\n")[0].replace(/^# /, ""),
+      html: marked.parse(content.text),
+    }));
+}
---
+++ src/client/modules/index.ts
@@ -0,0 +1,1 @@
+export { fetchPageContent } from "./fetchPageContent";
