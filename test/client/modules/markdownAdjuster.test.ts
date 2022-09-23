1cb8d8e5c799a357f50118cdba14969087d6384f
tom <toms@live.jp>
2022-09-24 07:47:21 +0900

    test for applyMermaid()

--- test/client/modules/markdownAdjuster.test.ts
+++ test/client/modules/markdownAdjuster.test.ts
@@ -1,8 +1,52 @@
 import { describe, it } from "mocha";
 import { strict as assert } from "assert";
+import { JSDOM } from "jsdom";
 
-describe("test", () => {
-  it("test is executed", () => {
-    assert(true);
+import { markdownAdjuster } from "../../../src/client/modules/";
+
+declare global {
+  var mermaid: any;
+}
+
+describe("markdownAdjuster", () => {
+  it("applyMermaid()", () => {
+    globalThis.mermaid = {
+      init: () => {},
+    };
+    globalThis.document = new JSDOM(`
+      <!DOCTYPE html>
+      <html>
+        <body>
+          <div id="article">
+            <pre class="test-target">
+              <code class="language-mermaid">
+                erDiagram
+                AAA ||--o{ BBB : relate
+              </code>
+            </pre>
+            <pre class="test-target">
+              <code class="language-mermaid">
+                erDiagram
+                CCC ||--o{ DDD : relate
+              </code>
+            </pre>
+          </div>
+        </body>
+      </html>
+    `).window.document;
+    markdownAdjuster.applyMermaid();
+
+    assert.equal(document.querySelectorAll(".test-target").length, 2);
+
+    assert(
+      Array.from(document.querySelectorAll(".test-target")).every((el) =>
+        el.classList.contains("language-mermaid")
+      )
+    );
+
+    assert.equal(
+      document.querySelectorAll(".test-target > div.mermaid").length,
+      2
+    );
   });
 });
