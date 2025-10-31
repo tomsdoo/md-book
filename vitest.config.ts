c5394a8f55b85bc360ef060f86c9471e5f5ace96
tomsdoo <toms@live.jp>
2025-10-31 20:26:17 +0900

    fix config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,43 @@
+import { playwright } from '@vitest/browser-playwright';
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+  test: {
+    projects: [
+      {
+        test: {
+          include: ["**/*.browser.test.{ts,js}"],
+          name: "browser",
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            headless: true,
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+        },
+        resolve: {
+          alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+          },
+        },
+      },
+      {
+        test: {
+          include: ["**/*.node.test.{ts,js}"],
+          name: "node",
+          environment: "node",
+        },
+        resolve: {
+          alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+          },
+        },
+      },
+    ],
   },
 });
